7bac79b828c1b7a0bcc56c4096b32c3140f5618a
Rollin972 <75938189+Rollin972@users.noreply.github.com>
2021-02-12 20:12:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function add_user()
+{
+    player1=document.getElementById("player1").value;
+    player2=document.getElementById("player2").value;
+
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+
+    window.location="game.html";
+}
